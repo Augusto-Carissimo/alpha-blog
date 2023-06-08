d4825a9c329a8ae2bd1318f30a10f35128fbbe1e
Augusto-Carissimo <augusto.carissimo@gmail.com>
2023-06-08 17:47:34 -0300

    Add edit and update routes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,32 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+      flash[:notice] = 'Article created'
+      redirect_to article_path(@article)
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = 'Article updated'
+      redirect_to article_path(@article)
+    else
+      render 'edit', status: :unprocessable_entity
+    end
+  end
 end
